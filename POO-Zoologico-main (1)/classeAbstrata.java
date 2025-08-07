acbd1f93a46756ad5e378f3a5c20624a3d23d775
Mikhail-edu <artur.sousa@escolar.ifrn.edu.br>
2025-08-07 10:57:24 -0300

    adicionar

---
+++ POO-Zoologico-main (1)/classeAbstrata.java
@@ -0,0 +1,22 @@
+abstract class Animal {
+    String nome; 
+
+    abstract void fazerSom ();
+
+
+    void dormir(){
+        System.out.println("dormindo...");
+    }
+
+class Cachorro extends Animal {
+
+    public Cachorro(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    void fazerSom() {
+        System.out.println(nome + " está latindo: Au au!");
+    }
+    
+}
